f653bf3e1152153613e62fb38d9763b2268e9474
Eduardo Silva <114937490+DuduzinPH@users.noreply.github.com>
2023-03-09 18:36:57 -0300

    Biblioteca de motores com shift
    Biblioteca de motores que serão controlados com shift register

---
+++ Códigos/Bibliotecas/shiftmotor/shiftmotor.ino
@@ -0,0 +1,22 @@
+/*
+============================ /// ===================================================
+          
+          
+          Biblioteca de Motores controlados por Shift Register
+          @GRELIFPB 
+          By Eduardo,Tomaz,Yan & Pedro.
+
+
+
+============================ /// ====================================================
+*/
+#include "shiftmotor.h"
+void setup() {
+  
+
+}
+
+void loop() {
+
+
+}
